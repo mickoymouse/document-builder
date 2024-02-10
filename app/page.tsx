48ab4e2a243290aec670963a0a02b5fb5b4c2933
mickoymouse <mckslvstr@gmail.com>
2024-02-11 01:50:39 +0800

    Implemented Layers
    1. Added layer items on left sidebar.

--- app/page.tsx
+++ app/page.tsx
@@ -151,7 +151,7 @@ export default function Page() {
 				handleActiveElement={handleActiveElement}
 			/>
 			<section className="h-full flex flex-row">
-				<LeftSidebar />
+				<LeftSidebar allShapes={Array.from(canvasObjects)} />
 				<Live canvasRef={canvasRef} />
 				<RightSidebar />
 			</section>
