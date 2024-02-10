c3473e379d2a47c14df0bced1b3346e361f5f2c0
mickoymouse <mckslvstr@gmail.com>
2024-02-11 01:29:35 +0800

    Added canvas and canvas operations
    1. Added Creating of basic shapes.
    2. Added moving of objects and resizing of objects.
    3. Added deleting of objects.
    4. Added reseting of whole canvas.

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,159 @@
 "use client";
+import LeftSidebar from "@/components/LeftSidebar";
 // import { CollaborativeApp } from "./CollaborativeApp";
 
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
+import RightSidebar from "@/components/RightSidebar";
+import { defaultNavElement } from "@/constants";
+import {
+	handleCanvasMouseDown,
+	handleCanvasMouseMove,
+	handleCanvasMouseUp,
+	handleCanvasObjectModified,
+	handleResize,
+	initializeFabric,
+	renderCanvas,
+} from "@/lib/canvas";
+import { handleDelete } from "@/lib/key-events";
+import { useMutation, useStorage } from "@/liveblocks.config";
+import { ActiveElement } from "@/types/type";
+import { fabric } from "fabric";
+import { useEffect, useRef, useState } from "react";
 
 export default function Page() {
+	const canvasRef = useRef<HTMLCanvasElement>(null);
+	const fabricRef = useRef<fabric.Canvas | null>(null);
+	const isDrawing = useRef(false);
+	const shapeRef = useRef<fabric.Object | null>(null);
+	const selectedShapeRef = useRef<string | null>("rectangle");
+	const activeObjectRef = useRef<fabric.Object | null>(null);
+
+	const canvasObjects = useStorage((root) => root.canvasObjects);
+
+	const syncShapeInStorage = useMutation(({ storage }, object) => {
+		if (!object) return;
+
+		const { objectId } = object;
+
+		const shapedData = object.toJSON();
+		shapedData.objectId = objectId;
+
+		const canvasObjects = storage.get("canvasObjects");
+
+		canvasObjects.set(objectId, shapedData);
+	}, []);
+
+	const deleteAllShapes = useMutation(({ storage }) => {
+		const canvasObjects = storage.get("canvasObjects");
+
+		if (!canvasObjects || canvasObjects.size === 0) return true;
+
+		for (const [key, value] of canvasObjects.entries()) {
+			canvasObjects.delete(key);
+		}
+
+		return canvasObjects.size === 0;
+	}, []);
+
+	const deleteShapeFromStorage = useMutation(({ storage }, objectId) => {
+		const canvasObjects = storage.get("canvasObjects");
+
+		canvasObjects.delete(objectId);
+	}, []);
+
+	const [activeElement, setActiveElement] = useState<ActiveElement>({
+		name: "",
+		value: "",
+		icon: "",
+	});
+
+	const handleActiveElement = (elem: ActiveElement) => {
+		setActiveElement(elem);
+
+		switch (elem?.value) {
+			case "reset":
+				deleteAllShapes();
+				fabricRef.current?.clear();
+				setActiveElement(defaultNavElement);
+				break;
+			case "delete":
+				handleDelete(fabricRef.current as any, deleteShapeFromStorage);
+				setActiveElement(defaultNavElement);
+				break;
+			default:
+				break;
+		}
+
+		selectedShapeRef.current = elem?.value as string;
+	};
+
+	useEffect(() => {
+		const canvas = initializeFabric({ canvasRef, fabricRef });
+
+		canvas.on("mouse:down", (options) => {
+			handleCanvasMouseDown({
+				options,
+				canvas,
+				selectedShapeRef,
+				isDrawing,
+				shapeRef,
+			});
+		});
+
+		canvas.on("mouse:move", (options) => {
+			handleCanvasMouseMove({
+				options,
+				canvas,
+				selectedShapeRef,
+				isDrawing,
+				shapeRef,
+				syncShapeInStorage,
+			});
+		});
+
+		canvas.on("mouse:up", () => {
+			handleCanvasMouseUp({
+				canvas,
+				isDrawing,
+				shapeRef,
+				activeObjectRef,
+				selectedShapeRef,
+				syncShapeInStorage,
+				setActiveElement,
+			});
+		});
+
+		canvas.on("object:modified", (options) => {
+			handleCanvasObjectModified({
+				options,
+				syncShapeInStorage,
+			});
+		});
+
+		window.addEventListener("resize", () => {
+			handleResize({ canvas: fabricRef.current });
+		});
+
+		return () => {
+			canvas.dispose();
+		};
+	}, []);
+
+	useEffect(() => {
+		renderCanvas({ fabricRef, canvasObjects, activeObjectRef });
+	}, [canvasObjects]);
+
 	return (
 		<main className="h-screen overflow-hidden">
-			<Navbar />
+			<Navbar
+				activeElement={activeElement}
+				handleActiveElement={handleActiveElement}
+			/>
 			<section className="h-full flex flex-row">
-				<Live />
+				<LeftSidebar />
+				<Live canvasRef={canvasRef} />
+				<RightSidebar />
 			</section>
 		</main>
 	);
--- components/Live.tsx
+++ components/Live.tsx
@@ -13,7 +13,11 @@ import FlyingReaction from "./reaction/FlyingReaction";
 import useInterval from "@/hooks/useInterval";
 import Cursor from "./cursor/Cursor";
 
-const Live = () => {
+type Props = {
+	canvasRef: React.MutableRefObject<HTMLCanvasElement | null>;
+};
+
+const Live = ({ canvasRef }: Props) => {
 	const others = useOthers();
 	const broadcast = useBroadcastEvent();
 
@@ -155,13 +159,14 @@ const Live = () => {
 
 	return (
 		<div
+			id="canvas"
 			className="relative h-screen w-full flex touch-none justify-center items-center text-center"
 			onPointerMove={handlePointerMove}
 			onPointerLeave={handlePointerLeave}
 			onPointerDown={handlePointerDown}
 			onPointerUp={handlePointerUp}
 		>
-			<h1>LIVE LIVE LIVE</h1>
+			<canvas ref={canvasRef} />
 
 			{reactions.map((reaction) => {
 				return (
